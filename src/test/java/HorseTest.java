8978a3b6baf29f3e86452d55833aff4ba3ece133
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-19 10:55:02 +0300

    add test  to constructor

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
 
+
+    @Test
+    void constructor() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10));
+    }
+
     @Test
     void getName() {
     }
