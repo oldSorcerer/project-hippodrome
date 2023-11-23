bd9c03aa2c4e4fa579bb43fad236af6027a5ff79
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-23 14:01:31 +0300

    refactor code style

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    void main() {
+    @Timeout(22)
+    void main1() {
     }
 }
