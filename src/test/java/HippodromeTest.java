7399a8bde2a30728c788598587a11ad6f8c4ce70
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-23 13:59:44 +0300

    add test from constructor

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,8 +1,32 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
+
+    @Test
+    void testConstructor_ShouldThrowIllegalArgumentEx_WhenArgsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    void testConstructor_ShouldThrowExTextMessage_WhenArgsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(null), "Horses cannot be null.");
+    }
+
+    @Test
+    void testConstructor_ShouldThrowIllegalArgumentEx_WhenArgsListIsEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(List.of()));
+    }
+
+    @Test
+    void testConstructor_ShouldThrowExTextMessage_WhenArgsListIsEmpty() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(List.of()), "Horses cannot be empty.");
+    }
 
     @Test
     void getHorses() {
