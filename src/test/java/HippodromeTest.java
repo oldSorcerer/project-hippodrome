06bf4999238d1139961f956e85b26391316d831d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-12-05 13:51:34 +0300

    refactor code and add new test methods

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,8 +16,10 @@ class HippodromeTest {
 
     @Test
     void testConstructor_ShouldThrowExTextMessage_WhenArgsNull() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Hippodrome(null), "Horses cannot be null.");
+        String expectedExceptionMessage = "Horses cannot be null.";
+
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+        assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
     @Test
@@ -24,19 +29,32 @@ class HippodromeTest {
 
     @Test
     void testConstructor_ShouldThrowExTextMessage_WhenArgsListIsEmpty() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Hippodrome(List.of()), "Horses cannot be empty.");
+        String expectedExceptionMessage = "Horses cannot be empty.";
+
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(List.of()));
+        assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
     @Test
     void getHorses() {
+        List<Horse> horses = IntStream.range(0, 30).mapToObj(i -> new Horse("" + i, i, i)).toList();
+        Hippodrome hippodrome = new Hippodrome(horses);
+
+        assertEquals(horses, hippodrome.getHorses());
     }
 
     @Test
     void move() {
+        List<Horse> horses = IntStream.range(0, 50).mapToObj(i -> Mockito.mock(Horse.class)).collect(Collectors.toList());
+
+        new Hippodrome(horses).move();
+
+        horses.forEach(horse -> Mockito.verify(horse, Mockito.times(1)).move());
     }
 
     @Test
     void getWinner() {
+
+//        new Horse()
     }
 }
